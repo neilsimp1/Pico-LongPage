d4ec6f1d83246d239217315e49191652db777da1
Neil Simpson <neilsimp1@gmail.com>
2017-07-29 17:26:27 -0400

    Adding ids

--- LongPage.php
+++ LongPage.php
@@ -8,32 +8,45 @@
 	* @version 1.0
 	*/ 
 	class LongPage extends AbstractPicoPlugin {
-		
-		public $config = [];
-		public $offset = 0;
-		public $page_number = 0;
-		public $total_pages = 1;
-		public $paged_pages = [];
-		
-		public function __construct(Pico $pico) {
+
+        private $isLongPage = false;
+
+        public function __construct(Pico $pico) {
 			parent::__construct($pico);
 		}
-		
-		public function onConfigLoaded(&$settings) {
-			
-		}
-		
-		public function onPagesLoaded(&$pages, &$currentPage, &$previousPage, &$nextPage) {
-			
-		}
-		
-		public function onPageRendering(&$twig, &$twigVariables, &$templateName) {
-			
-		}
-		
-		public function onRequestUrl(&$url) {
-			
-		}
+
+        public function onMetaParsed(array &$meta) {
+            if(isset($meta["islongpage"]) && $meta["islongpage"]) $this->isLongPage = true;
+        }
+
+        public function onContentParsing(&$rawContent) {
+            if($this->isLongPage){                
+                $rawContent = $this->addIdsToH2s($rawContent);
+            }
+            $asd= 123;
+        }
+
+        private function addIdsToH2s($rawContent) {
+            $newContent = "";
+            foreach(preg_split("/((\r?\n)|(\r\n?))/", $rawContent) as $line){
+                if(count($line > 3) && $line && $line[0] === "#" && $line[1] === "#"){
+                    $properTitle = substr($line, 3);
+                    $id = $this->toCamelCase($properTitle);
+                    $newContent .= "<h2 id=\"$id\">$properTitle</h2>\n";
+                }
+                else $newContent .= $line."\n";
+            }
+
+            return $newContent;
+        }
+
+        private function toCamelCase($str) {
+            $str = str_replace("-", "", ucwords($str, "-"));
+            $str = str_replace(" ", "", ucwords($str, " "));
+            $str = lcfirst($str);
+
+            return $str;
+        }
 	}
 
 ?>
